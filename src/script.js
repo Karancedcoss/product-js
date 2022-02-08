c5979cdfcc96894c1bbd0ba57b8b5aab14544658
Karan Gupta Sir <karangupta4517@gmail.com>
2022-02-08 13:30:36 +0530

    commit 2 error solve

--- src/script.js
+++ src/script.js
@@ -1,31 +1,44 @@
- arr=[];
+array=[];
 
 function Function () {
     var id = document.getElementById('ID').value;
     var price = document.getElementById('Price').value;
     var name = document.getElementById('Name').value;
-    var output= {};
     console.log("correct");
-    output = logic (id, price, name);
-    display(output);
+    logic (id, price, name);
+    display(array);
 }
-function logic(id, price, name) {
-    
-    obj = {};
+
+function logic(id, price, name) 
+{
+    for (var i = 0; i < array.length; i++)
     {
-        obj["id"] = id;
-        obj["name"] = name;
-        obj["price"] = price; 
-        arr.push(obj);
-        console.log(arr);
-        return arr;  
+        console.log(array[i]);
+        if (array[i].id == id)
+        {
+            alert("YOUR ID IS SAME");
+            return;
+        }
     }
+    obj = {};
+    obj["id"] = id;
+    obj["name"] = name;
+    obj["Price"] = price; 
+    array.push(obj);
 }
+
 function display(result) {
     var text=document.getElementById("table1");
-    text.innerHTML+=`<tr> 
-    <td>${result[0].id}</td>
-    <td>${result[0].name}</td>
-    <td>${result[0].price}</td>
- </tr>`;
-}  
+    text.innerHTML="<tr>\
+    <th>Product Id</th>\
+    <th>Product Name</th>\
+    <th>Product Price</th>\
+    </tr>";
+    for (let i=0; i<result.length; i++) {
+        text.innerHTML+='<tr>\
+        <td>' + result[i].id + '</td>\
+        <td>' + result[i].name + '</td>\
+        <td>' + result[i].Price + '</td>\
+    </tr>';
+    }
+}
